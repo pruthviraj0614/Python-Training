502ccac88f1b953d9af44d22f40d111a430277ef
pruthviraj0614 <pruthviraj0614@gmail.com>
2025-07-17 11:01:36 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+a="abccba"
+b=a[ : :-1]
+if (a==b):
+  print("its palindrome")
+else:
+   print("its not palindrome") 
+
+def palindrome(p):
+   return p==p[::-1]
+word=input("Enter the word") 
+print("Yes its palindrome")
+
